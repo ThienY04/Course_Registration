eb55f07c69d8b4b294f0a42e78aba2f74769e0c3
ThienY04 <ntrthieny@gmail.com>
2026-03-24 08:04:35 +0700

    Update StudentDetailsService.java

--- src/main/java/com/example/Course_Registration/service/StudentDetailsService.java
+++ src/main/java/com/example/Course_Registration/service/StudentDetailsService.java
@@ -1,7 +1,7 @@
 package com.example.Course_Registration.service;
 
-import com.example.Course_Registration.model.Student; // Import class Entity Student
-import com.example.Course_Registration.repository.StudentRepository; // Import Repository
+import com.example.Course_Registration.model.Student; 
+import com.example.Course_Registration.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,22 +12,20 @@ import org.springframework.stereotype.Service;
 public class StudentDetailsService implements UserDetailsService {
 
     @Autowired
-    private StudentRepository studentRepository; // Giả sử bạn đã có Repository này
+    private StudentRepository studentRepository; 
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // Tìm student trong DB
         Student student = studentRepository.findByUsername(username);
 
         if (student == null) {
             throw new UsernameNotFoundException("Không tìm thấy sinh viên!");
         }
 
-        // Trả về đối tượng User của Spring Security
         return org.springframework.security.core.userdetails.User
                 .withUsername(student.getUsername())
                 .password(student.getPassword())
-                .roles("USER") // Hoặc lấy từ bảng student_role của bạn
+                .roles("USER") 
                 .build();
     }
 }
